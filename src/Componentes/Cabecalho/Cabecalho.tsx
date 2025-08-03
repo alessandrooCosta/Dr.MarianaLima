c11cfe0d23bc20e7d6f11a5020aa938550e68ac4
Alessandro <alessandrocosta103@gmail.com>
2025-08-03 18:48:22 -0300

    atualiza cabeçalho

--- src/Componentes/Cabecalho/Cabecalho.tsx
+++ src/Componentes/Cabecalho/Cabecalho.tsx
@@ -1,20 +1,35 @@
-
-import { Link } from 'react-router-dom'
-import './Cabecalho.css'
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import './Cabecalho.css';
 
 function Cabecalho() {
+  const [menuAberto, setMenuAberto] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuAberto(!menuAberto);
+  };
+
   return (
     <header>
       <Link to="/">
-        <div className="logo"><img src="\imagens\fotos\logo_cabecalho.jpg" alt="Logo" /></div>
+        <div className="logo">
+          <img src="/imagens/fotos/logo_cabecalho.jpg" alt="Logo" />
+        </div>
       </Link>
-      <nav>
-        <Link to="/sobre"><a>Sobre</a></Link>
-        <Link to="/servicos"><a>Serviços</a></Link>
-        <Link to="/contato"><a>Contato</a> </Link>
-      </nav>
+
+      <div className={`nav ${menuAberto ? 'ativo' : ''}`}>
+        <Link to="/sobre" onClick={() => setMenuAberto(false)}>Sobre</Link>
+        <Link to="/servicos" onClick={() => setMenuAberto(false)}>Serviços</Link>
+        <Link to="/contato" onClick={() => setMenuAberto(false)}>Contato</Link>
+      </div>
+
+      <div className="hamburguer" onClick={toggleMenu}>
+        <span className="linha"></span>
+        <span className="linha"></span>
+        <span className="linha"></span>
+      </div>
     </header>
-  )
+  );
 }
 
-export default Cabecalho
+export default Cabecalho;
