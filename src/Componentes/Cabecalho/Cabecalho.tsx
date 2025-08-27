fafcc0a7f475ca589bfe4e82080d7da401cbeee7
Alessandro <alessandrocosta103@gmail.com>
2025-08-27 01:20:40 -0300

    atualiza

--- src/Componentes/Cabecalho/Cabecalho.tsx
+++ src/Componentes/Cabecalho/Cabecalho.tsx
@@ -17,12 +17,12 @@ function Cabecalho() {
       </Link>
       <div className={`nav ${menuAberto ? 'ativo' : ''}`} aria-label="Menu de navegação">
         <Link to="/" onClick={() => setMenuAberto(false)} className="nav-icon">
-          <img src="/imagens/icones/home.svg" alt="Icone da página principal" />
+          <img src="/imagens/icones/home.svg" alt="Logo da Dra. Mariana Lima" />
         </Link>
         <Link to="/sobre" onClick={() => setMenuAberto(false)}>Sobre</Link>
         <Link to="/servicos" onClick={() => setMenuAberto(false)}>Serviços</Link>
       </div>
-      <div className="hamburguer" onClick={toggleMenu} aria-label="Menu">
+      <div className="hamburguer" onClick={toggleMenu} aria-label="Menu principal">
         <span className="linha"></span>
         <span className="linha"></span>
         <span className="linha"></span>
