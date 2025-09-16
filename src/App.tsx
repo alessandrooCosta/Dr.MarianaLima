8b2419edd556d8fb2a54039b78fb2cad097bd95d
Alessandro <alessandrocosta103@gmail.com>
2025-09-15 23:09:50 -0300

    atualiza sitemap.xml

--- src/App.tsx
+++ src/App.tsx
@@ -84,37 +84,3 @@ export default function App() {
     </div>
   )
 }
-
-
-
-/* 
-
-import './App.css'
-
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Cabecalho from './Componentes/Cabecalho/Cabecalho'
-import Home from './Paginas/Home'
-import Sobre from './Paginas/Sobre'
-import Servicos from './Paginas/Servicos'
-import Rodape from './Componentes/Rodape/Rodape'
-
-function App() {
-
-  return (
-    <div>
-      <BrowserRouter>
-        <Cabecalho />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/sobre" element={<Sobre />} />
-          <Route path="/servicos" element={<Servicos />} />
-        </Routes>
-        <Rodape />
-      </BrowserRouter>
-    </div>
-  )
-}
-
-export default App
-
-*/
